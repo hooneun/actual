d0bd10cdfae3494f85ec4c2ff0c4ed1ab218e2dd
hooneun <kimhoon0316@gmail.com>
2023-03-15 22:51:42 +0900

    .

--- lib/product/component/product_card.dart
+++ lib/product/component/product_card.dart
@@ -119,13 +119,19 @@ class ProductCard extends ConsumerWidget {
           ),
         ),
         if (onSubtract != null && onAdd != null)
-          _Footer(
-            total: (basket.firstWhere((e) => e.product.id == id).count *
-                    basket.firstWhere((e) => e.product.id == id).product.price)
-                .toString(),
-            count: basket.firstWhere((e) => e.product.id == id).count,
-            onSubtract: onSubtract!,
-            onAdd: onAdd!,
+          Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: _Footer(
+              total: (basket.firstWhere((e) => e.product.id == id).count *
+                      basket
+                          .firstWhere((e) => e.product.id == id)
+                          .product
+                          .price)
+                  .toString(),
+              count: basket.firstWhere((e) => e.product.id == id).count,
+              onSubtract: onSubtract!,
+              onAdd: onAdd!,
+            ),
           ),
       ],
     );
@@ -162,6 +168,7 @@ class _Footer extends StatelessWidget {
         Row(
           children: [
             renderButton(icon: Icons.remove, onTap: onSubtract),
+            const SizedBox(width: 8.0),
             Text(
               count.toString(),
               style: TextStyle(
@@ -169,6 +176,7 @@ class _Footer extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
+            const SizedBox(width: 8.0),
             renderButton(icon: Icons.add, onTap: onAdd),
           ],
         )
